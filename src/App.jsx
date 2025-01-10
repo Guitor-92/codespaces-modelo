5becb289079340202561a2150ca66f0706ae1267
Guilherme Vitor Neves Pereira <96185124+Guitor-92@users.noreply.github.com>
2025-01-10 15:14:19 +0000

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -24,16 +24,17 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Guilherme Vitor",
+  title: "Desenvolvedor & Administrador",
+  email: "vitorguilh@hotmail.com",
+  gitHub: "Guitor-92",
+  instagram: "Guilherm",
+  linkedIn: "Guilherme Vitor",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
+
 
 const primaryColor = "#4E567E";
 const secondaryColor = "#D2F1E4";
